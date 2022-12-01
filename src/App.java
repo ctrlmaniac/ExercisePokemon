fd76ad2cb2a5622bf3872a2df24ad9c91c10941e
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-01 10:57:14 +0100

    refactor Pokemon models

--- src/App.java
+++ src/App.java
@@ -1,5 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        DatabaseManager db = new DatabaseManager();
+        db.resetTable("pokemon");
     }
 }
--- src/persistance/Credentials.java
+++
@@ -1,8 +0,0 @@
-package persistance;
-
-public class Credentials {
-    public static String dbAddress = "jdbc:mysql://localhost:3306/pokemon";
-    public static String user = "root";
-    public static String password = "root";
-    public static String className = "com.mysql.cj.jdbc.Driver";
-}
