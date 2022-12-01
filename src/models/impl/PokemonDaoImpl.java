4f37f676bb0c63227b5a6b582231fbb9d82f9f46
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-01 11:09:32 +0100

    add creation of pokemon table

--- src/models/impl/PokemonDaoImpl.java
+++ src/models/impl/PokemonDaoImpl.java
@@ -5,8 +5,30 @@ import java.util.List;
 
 import models.Pokemon;
 import models.dao.PokemonDao;
+import persistance.DatabaseManager;
 
 public class PokemonDaoImpl implements PokemonDao {
+    private DatabaseManager db;
+    private Connection conn;
+
+    public PokemonDaoImpl() {
+        db = new DatabaseManager();
+        conn = db.connection;
+        createTable();
+    }
+
+    private void createTable() {
+        db.resetTable("pokemon");
+        db.createTable("""
+                CREATE TABLE pokemon.pokemon(
+                    `id` INT PRIMARY KEY AUTO_INCREMENT,
+                    `name` VARCHAR(50),
+                    `elementType` VARCHAR(50),
+                    `level` INT
+                )
+                """);
+    }
+
     @Override
     public Pokemon createPokemon(Pokemon pokemon) {
         // TODO Auto-generated method stub
