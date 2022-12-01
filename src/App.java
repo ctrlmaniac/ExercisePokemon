973f4cff7509257c2e5027a44480dcf16f134de0
Davide Di Criscito <davide.dicriscito@gmail.com>
2022-12-01 11:03:36 +0100

    add initial implementation of PokemonDao

--- src/App.java
+++ src/App.java
@@ -1,6 +1,4 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        DatabaseManager db = new DatabaseManager();
-        db.resetTable("pokemon");
+    public static void main(String[] args) {
     }
 }
---
+++ src/models/impl/PokemonDaoImpl.java
@@ -0,0 +1,40 @@
+package models.impl;
+
+import java.sql.Connection;
+import java.util.List;
+
+import models.Pokemon;
+import models.dao.PokemonDao;
+
+public class PokemonDaoImpl implements PokemonDao {
+    @Override
+    public Pokemon createPokemon(Pokemon pokemon) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Pokemon getPokemonById(int id) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Pokemon> getAllPokemon() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updatePokemon(Pokemon pokemon) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deletePokemon(Pokemon pokemon) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
